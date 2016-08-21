151ec9816cf2da7882007de93185176dcbe6ee93
Jakub Badowski <jakub.badowski@gmail.com>
2016-08-21 21:14:39 +0200

    new file

---
+++ WifiReset.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Your driver name
+DRIVER="iwl3945"
+
+echo "WiFi driver reset - by Badyl"
+echo
+
+if sudo rmmod $DRIVER
+	then
+	echo "driver off"
+fi
+
+sleep 3
+
+if sudo modprobe $DRIVER
+	then
+	echo "driver on"
+fi
+
+sleep 3
